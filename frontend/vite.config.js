9177ef7ad29935bc823c2d569e73237ca39df494
Osman <fxosman7@gmail.com.com>
2026-01-08 03:28:00 +0600

    for deply

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
-  plugins: [react() , tailwindcss(),],
+  base: process.env.VITE_BASE_PATH || "/University_Final_Project2y/",
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
   server: {
-    port: 5173
-  }
+    port: 5173,
+  },
 });
